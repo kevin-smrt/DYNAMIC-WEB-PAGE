c15c21c5d6404f9bbad90d072aad81c1154a7b96
kevin_smrt <kevinsanmartin@live.fr>
2021-08-25 20:42:28 +0200

    add main controller

--- app/router.js
+++ app/router.js
@@ -1,13 +1,13 @@
 // Récupère seulement la partie Router de express
 const { Router } = require("express");
+// Importation du controller principal
+const mainController = require('./controllers/mainController');
 
 // Instancie le router
 const router = Router();
 
 // La route par défaut
-router.get('/', (req, res) => {
-    res.send("Hello world!");
-});
+router.get('/', mainController.homePage);
 
 // Exportation du router pour pouvoir l'utiliser dans index.js
 module.exports = router;
