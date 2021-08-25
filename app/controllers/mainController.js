c2c45e999f66c8728f7a058f3b85c382fc7c2526
kevin_smrt <kevinsanmartin@live.fr>
2021-08-25 21:47:37 +0200

    add main controller route

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -3,10 +3,14 @@ const Product = require('../models/product');
 
 // Initialisation de l'objet mainController qui sera exporté
 const mainController = {
+    // Méthode pour la page d'accueil
     homePage: async (req, res) => {
         try {
-            
+            // Récuperation de tout les produits
+            products = await Product.findAll();
+            res.render('index', {products});
         } catch (err) {
+            // Gestion en cas d'erreur
             console.trace(err);
             res.status(500).send(err);
         }
